02faacd5bc434951856238a1618b3a115a77836d
SuitesSoftwareCreator <119679986+AlexnomSoftwareCreator@users.noreply.github.com>
2022-12-02 16:16:43 +0200

    SuitesAntivirus 1.0.bat
    main file

---
+++ SuitesAnti 1.0.bat
@@ -0,0 +1,27 @@
+@Echo off
+Color 40
+Title SuitesAnti virusDeleter tool 1.0
+Echo Hello! welcome to SuitesAnti VirusDeleter Tool 1.0
+Echo This is tool to delete virus on your computer:)
+Pause
+Echo Press any key to start scan and virus deleter
+Del Noescape.exe
+Del Spyheriff.exe
+Del Micro virus.exe
+Del Micro virus.bat
+Del Trojan horse.exe
+Del Trojan horse.bat
+Del Virus.exe
+Del Virus.bat
+Del Antivirus Installer.exe
+Del Antivirus.exe
+Del Time.exe
+Del Magicreverssi.zip
+Del Wannacry.exe
+Del Petya.exe
+Del Creeper.com
+Del Winpcap Installer.exe
+Echo Copyright@ 2022 SuitesAnti The Command line antivirus software Official SuitesAnti 1.0
+Echo * Complete
+Echo Press any key to exit program
+Pause
